f409cefbae1491d36324dd7ad7021c82f6c051b1
michellevp25 <michellevalentinapurnomo@gmail.com>
2020-07-27 19:12:07 +0700

    addresource

--- sketch.js
+++ sketch.js
@@ -1,7 +1,41 @@
+let x=100;
+let y = 100;
+let poseNet;
+let pose;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	webcam = createCapture(VIDEO);
+	webcam.hide();
+	poseNet = ml5.poseNet(webcam, function ()
+	{
+		console.log(poseNet);
+		console.log("model loaded");
+	}
+	);
+	poseNet.on("pose",getPoses);
 }
 
-function draw() {
 
+function draw() {
+	background(255, 0, 0)
+	
+	image(webcam, 0, 0);
+
+	if(pose)
+	{
+		for(i in pose.pose)
+		{
+			if(pose.pose[i].confidence > 0.90)
+			{
+				ellipse(pose.pose[i].x,pose.pose[i].y,25);
+			}				
+		}	
+	}
+}
+
+function getPoses(results)
+{
+	pose = results[0];
+	console.log(pose);
 }
